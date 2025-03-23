705ded90ac8e2c889f125f2b16a843f92d13a547
Ian Goller <ian.goller915@gmail.com>
2025-03-23 08:04:03 -0400

    add static dir with css and image dir add copy dir function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
 from textnode import *
+import os
+import shutil
+
+def copy_directory_contents(source, destination):
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    shutil.copytree(source, destination)
+
 
 def main():
-    text_node1 = TextNode("Alt Text", TextType.BOLD, "https://www.boot.dev")
-    print(text_node1)
+    copy_directory_contents("/home/iangoller/bootdev/static_site_gen/static","/home/iangoller/bootdev/static_site_gen/public")
 
 main()
