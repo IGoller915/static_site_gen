884b6fd25fabe2655bdd4e5a561f389810719cef
Ian Goller <ian.goller915@gmail.com>
2025-03-24 20:09:04 -0400

    reconfigured to deploy to github page

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from textnode import *
 import os
 import shutil
 from page_generation_functions import *
+import sys
 
 def copy_directory_contents(source, destination):
     if os.path.exists(destination):
@@ -10,7 +11,13 @@ def copy_directory_contents(source, destination):
 
 
 def main():
-    copy_directory_contents("/home/iangoller/bootdev/static_site_gen/static","/home/iangoller/bootdev/static_site_gen/public")
-    generate_page_recursive("/home/iangoller/bootdev/static_site_gen/content", "/home/iangoller/bootdev/static_site_gen/template.html")
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+    print (basepath)
+
+    copy_directory_contents("/home/iangoller/bootdev/static_site_gen/static","/home/iangoller/bootdev/static_site_gen/docs")
+    generate_page_recursive("/home/iangoller/bootdev/static_site_gen/content", "/home/iangoller/bootdev/static_site_gen/template.html", basepath)
     
 main()
--- src/page_generation_functions.py
+++ src/page_generation_functions.py
@@ -10,8 +10,8 @@ def extract_title(markdown):
     stripped_title = title_text.strip("# \n")
     return stripped_title
 
-def generate_page(from_path, template_path):
-    dest_file_path = from_path.replace("content", "public").replace(".md", ".html")
+def generate_page(from_path, template_path, basepath):
+    dest_file_path = from_path.replace("content", "docs").replace(".md", ".html")
     print(f"Generating page from {from_path} to {dest_file_path} using {template_path}")
     markdown_file = open(from_path)
     markdown_text = markdown_file.read()
@@ -21,20 +21,22 @@ def generate_page(from_path, template_path):
     content_html_text = content_html_node.to_html()
     title = extract_title(markdown_text)
     title_template = template_text.replace("{{ Title }}", title)
-    final_html = title_template.replace("{{ Content }}", content_html_text)
+    # basepath_links_html = title_template.replace("href=\"/", f"href=\"{basepath}").replace("src=\"/", f"src=\"{basepath}")
+    replace_content_html = title_template.replace("{{ Content }}", content_html_text)
+    final_html = replace_content_html.replace("href=/", f"href={basepath}").replace("src=/", f"src={basepath}")
     output_file = open(dest_file_path, "w")
     output_file.write(final_html)
 
-def generate_page_recursive(from_path, template_path):
-    dest_dir_path = from_path.replace("content", "public")
+def generate_page_recursive(from_path, template_path, basepath):
+    dest_dir_path = from_path.replace("content", "docs")
     ls = os.listdir(from_path)
     for item in ls:
         item_path = os.path.join(from_path, item)
         if os.path.isfile(item_path):
-            generate_page(item_path, template_path)
+            generate_page(item_path, template_path, basepath)
         else:
             os.mkdir(os.path.join(dest_dir_path, item)) #if item is a directory, creates the directory in the public folder
-            generate_page_recursive(item_path, template_path)
+            generate_page_recursive(item_path, template_path, basepath)
     
 
 # generate_page_recursive("/home/iangoller/bootdev/static_site_gen/content/", "der", "der")
